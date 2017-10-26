ee15913d25d36d0b1ec339343ac632a8a2215e28
Julia <Julia@JULIA->
2017-10-26 22:21:00 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Models/MeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    // Модели, возвращенные действиями MeController.
+    public class GetViewModel
+    {
+        public string Hometown { get; set; }
+    }
+}
